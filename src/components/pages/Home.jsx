361a560cbbd8a2d7be1f9821351d8fa6b6e74597
thy-Jundy <thy.jundy@gmail.com>
2024-01-10 22:34:06 +0800

    Setup - React Router, Ag Grid, Tauri Config, Tailwind, Prettier

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const Home = (props) => {
-  const [message, setMessage] = useState('Hello Worlds!');
+  const [message, setMessage] = useState('Hello World!');
   return <h1>{message}</h1>;
 };
 
