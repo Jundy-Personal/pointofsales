f0046020fb9bcf0829fc1e5b0a25db41db8ec5b7
blue-Jundy <dimasuay.jm08@gmail.com>
2024-01-13 03:19:28 +0800

    #5 [Atoms] Optimize buttons for common logic functions

--- src/components/atoms/commonFunctions/buttons.jsx
+++ src/components/atoms/commonFunctions/buttons.jsx
@@ -2,21 +2,19 @@ import { useEffect, useState } from 'react';
 
 export const useButtonStyles = (size, block) => {
   const sizes = {
-    small: 'py-2 text-xs',
-    large: 'py-3 text-sm',
+    small: 'px-4 py-2 text-xs',
+    medium: 'px-5 py-2 text-sm',
   };
 
   if (size && !sizes.hasOwnProperty(size)) {
     console.error(`Property ${size} does not exist in the list of sizes.`);
   }
 
-  const [buttonSize, setButtonSize] = useState('');
+  const [buttonSize, setButtonSize] = useState('px-5 py-2.5 text-sm');
   const [blockDesign, setBlockDesign] = useState('');
 
   useEffect(() => {
-    if (!size) {
-      setButtonSize(sizes.large);
-    } else {
+    if (size) {
       setButtonSize(sizes[size]);
     }
   }, [size]);
@@ -31,5 +29,5 @@ export const useButtonStyles = (size, block) => {
 };
 
 export const useCommonCss = () => {
-  return 'px-4 hover:shadow-md hover:-translate-y-0.5 active:translate-y-0 rounded-sm duration-300';
+  return 'hover:shadow-md hover:-translate-y-0.5 active:translate-y-0 rounded-sm duration-300';
 };
